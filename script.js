b315a398366196d1348718f59bc8f88d800f1fc3
AnandVyas01 <anandvyas1114@gmail.com>
2022-04-28 07:54:21 +0000

    changes made to html, css and js files and added functionality to calculate SI

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal=document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    // user cannot enter 0 or -ve values
+    if(principal==0 || principal<0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else{
     
+    // calculatring interest
+    var interest = principal * years * rate /100;
+    // var interest_amount=parseInt(interest)+parseInt(principal);
+    var y = new Date()
+    var year=y.getFullYear()+parseInt(years);
+
+    // p = document.getElementById("principal").value;
+    // resulting text
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"   
+    }     
 }
-        
+// function for showing rate value 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
+
+}
+
+     
